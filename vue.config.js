493da19ef9f750d6b9338d0c6943402dfa076a56
Afif-Ghifari <aghifari1204@gmail.com>
2023-09-23 21:31:06 +0700

    deploy again

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
+module.exports = 
+{
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Vue-recipe-book/'
     : '/'
